47b6927b7be149499bfb38c80a708b61fcbfd363
shadow <shadow@MacBook-Pro-Shadow.local>
2020-03-28 00:06:04 +0300

    Fixed title

--- resources/views/template.php
+++ resources/views/template.php
@@ -12,7 +12,7 @@
 Остальные просто не найдут.
 -->
 <head>
-    <title>Test title</title>
+    <title>shadowlabs.</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
