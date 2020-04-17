d56c5c90640b959c48c96d8b54207000358c3bdc
shadow <shadow@MacBook-Pro-Shadow.local>
2020-04-17 23:26:14 +0300

    Added data alert

--- resources/views/tasks/task-7.php
+++ resources/views/tasks/task-7.php
@@ -77,6 +77,12 @@ HEREDOC;
             <div class="col-12" data-aos="fade-up">
                 <h3 class="heading-underlined">Результат</h3>
             </div>
+            <div class="col-md-6">
+                <div class="alert" data-aos="fade-up">
+                    <svg aria-hidden="true" focusable="false" data-prefix="fal" data-icon="info-circle" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="svg-inline--fa fa-info-circle fa-w-16 fa-3x"><path fill="currentColor" d="M256 40c118.621 0 216 96.075 216 216 0 119.291-96.61 216-216 216-119.244 0-216-96.562-216-216 0-119.203 96.602-216 216-216m0-32C119.043 8 8 119.083 8 256c0 136.997 111.043 248 248 248s248-111.003 248-248C504 119.083 392.957 8 256 8zm-36 344h12V232h-12c-6.627 0-12-5.373-12-12v-8c0-6.627 5.373-12 12-12h48c6.627 0 12 5.373 12 12v140h12c6.627 0 12 5.373 12 12v8c0 6.627-5.373 12-12 12h-72c-6.627 0-12-5.373-12-12v-8c0-6.627 5.373-12 12-12zm36-240c-17.673 0-32 14.327-32 32s14.327 32 32 32 32-14.327 32-32-14.327-32-32-32z" class=""></path></svg>
+                    <p>Данные, находящиеся в таблицах доступны для изменения кому угодно. Владелец приложения не несет ответственности за содержимое таблиц.</p>
+                </div>
+            </div>
             <div class="col-12" data-aos="fade-up">
                 <h6>Использованный SQL</h6>
                 <?php
@@ -288,13 +294,6 @@ HEREDOC;
         background-color: rgba(243, 6, 61, .5);
         border: 2px solid #ff1032;
     }
-
-    .alert.warning {
-        background-color: rgba(255, 94, 0, 0.5);
-        border: 2px solid #ff682e;
-    }
-
-
 
     .alert > svg {
         margin-top: -60px;
